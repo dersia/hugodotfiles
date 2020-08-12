2cf4e814662707dc4c3fe2f0d1d4261da1509b8e
dersia <info@dersia.org>
2020-08-12 19:30:26 +0200

    test3

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" && /home/linuxbrew/.linuxbrew/bin/brew install hugo
+CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/codespace/.profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+brew install hugo
