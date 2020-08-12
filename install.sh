724f175a43af726b9ab738baa11e32ee1bd090e3
dersia <info@dersia.org>
2020-08-12 19:06:14 +0200

    changed install script2

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
-CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-/home/linuxbrew/.linuxbrew/bin/brew install hugo
+CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" && /home/linuxbrew/.linuxbrew/bin/brew install hugo
