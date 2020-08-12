d8a82daa131c00dd62e944afdd4b83dab290549e
dersia <info@dersia.org>
2020-08-12 18:54:17 +0200

    changed install script

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
-sudo apt-get install hugo
+CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/codespace/.profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
